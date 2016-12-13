09eb5b008b0b51a110bfe3312917c7a987c3b890
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2016-12-13 16:24:30 -0700

    I created an oEmcee class so everything is carried around in one object with methods for running the emcee, making plots etc.

--- fit_tser_emcee.py
+++ fit_tser_emcee.py
@@ -7,9 +7,10 @@ import corner
 
 class sinModel:
     """ Simple sinusoidal model for fitting light curve.
+        y(t) = A1 cos(2pi(t-t1)/tau) + A2 cos(2pi(t-t2)/0.5tau) + Bt + C'
         p[0] = A, p[1] = t0, p[2]=tau, p[3]=B, p[4]=C
     """
-    def __init__(self):
+    def __init__(self,priorArray=None):
         self.name = "Sinusoidal with Baseline"
         self.pnames = ['A$_1$', 't$_1$', r'$\tau$', 'B', 'C']
         self.formula = 'A1 cos(2pi(t-t1)/tau) + Bt + C'
@@ -48,7 +49,132 @@ def lnprob(params, x, y, yerr, model):
     else:
         return -np.inf
 
-def prepEmcee(showGuess=False):
+class oEmcee():
+    """ An MCMC object class to initialize and run an MCMC model
+    
+    Attributes
+    ----------
+    model: obj
+        Model for the fitting
+    x: arr
+        X values
+    y: arr
+        Y values
+    yerr: arr
+        Y errors
+    guess: arr
+        Guess for the initial values of the model
+    guessSig: arr
+        Initial Gaussian sigmas to start walkers around the guess
+    nwalkers: int, optional
+        Number of walkers in MCMC fit
+    """
+    
+    def __init__(self,model,x,y,yerr,guess,guessSig,nwalkers=50):
+        self.model = model
+        self.x = x
+        self.y = y
+        self.yerr = yerr
+        self.guess = guess
+        self.guessSig = guessSig
+        self.ndim = model.ndim
+        self.nwalkers = nwalkers
+        self.p0 = self.makeWalkers()
+        self.sampler = emcee.EnsembleSampler(self.nwalkers,self.ndim,lnprob,
+                                             args=[self.x,self.y,self.yerr,self.model])
+        self.hasRun = False
+    
+    def makeWalkers(self):
+        """ Makes the MCMC walkers """
+        p0 = []
+        for i in range(self.nwalkers):
+            p0.append(self.guess + np.random.normal(0,1.0,self.ndim) * self.guessSig)
+        return p0
+    
+    def showGuess(self):
+        """ Shows the guess against the input """
+        plt.close('all')
+        fig, ax = plt.subplots()
+        ax.errorbar(self.x,self.y,yerr=self.yerr,fmt='o')
+        ax.plot(self.x,self.model.evaluate(self.x,self.guess))
+        fig.show()
+    
+    def runMCMC(self,nBurn=500,nRun=1500):
+        """ Runs the MCMC
+        Example::
+        mcObj = oEmcee(model,x,y,yerr,guess,guessSig)
+        mcObj.runMCMC()
+        mcObj.plotMCMC()
+        
+        """
+        if nBurn != 0:
+            self.pos, prob, state = self.sampler.run_mcmc(self.p0,nBurn)
+            self.sampler.reset()
+        else:
+            self.sampler.reset()
+        self.pos, prob, state = self.sampler.run_mcmc(self.pos,nRun)
+        self.getResults()
+        self.hasRun= True
+    
+    def getResults(self,lowPercent=15.9,highPercent=84.1):
+        """ Get results from the chains in the sampler object """
+        lower,medianV,upper = [], [], []
+        for i in range(self.ndim):
+            flatChain = self.sampler.flatchain[:,i]
+            lower.append(np.percentile(flatChain,lowPercent))
+            medianV.append(np.median(flatChain))
+            upper.append(np.percentile(flatChain,highPercent))
+        
+        self.lower = np.array(lower)
+        self.medianV = np.array(medianV)
+        self.upper = np.array(upper)
+    
+    def runCheck(self):
+        """A check that the MCMC has been run. If not, it runs"""
+        if self.hasRun == False:
+            print("Note: MCMC hasn't run yet and is now executing")
+            self.runMCMC()
+    
+    def doCorner(self):
+        """ Runs a corner plot """
+        self.runCheck()
+        
+        fig = corner.corner(self.sampler.flatchain,labels=self.model.pnames,auto_bars=False,
+                            quantiles=[0.159,0.841],show_titles=True)
+        fig.savefig('plots/corner.pdf')
+    
+    def plotMCMC(self):
+        """ Plots the MCMC histograms """
+        self.runCheck()
+        
+        plt.close('all')
+        fig, axArr = plt.subplots(1,self.ndim,figsize=(17,5))
+        for i, ax in enumerate(axArr):
+            postData = self.sampler.flatchain[:,i]
+            ax.hist(postData,100,histtype='step')
+            ax.set_xlabel(self.model.pnames[i])
+            lowLimit = np.percentile(postData,5)
+            highLimit = np.percentile(postData,95)
+            showLow = lowLimit - (highLimit - lowLimit)
+            showHigh = highLimit + (highLimit - lowLimit)
+            ax.set_xlim(showLow,showHigh)
+            ax.get_yaxis().set_visible(False)
+        
+        fig.show()
+        
+    def plotHistory(self):
+        """ Shows the chain history """
+        self.runCheck()
+        plt.close('all')
+        fig, axArr = plt.subplots(self.ndim,figsize=(10,10))
+        for i, ax in enumerate(axArr):
+            for j in range(self.sampler.chain.shape[0]):
+                chainDat = self.sampler.chain[j,:,i]
+                ax.plot(chainDat)
+                ax.set_ylabel(self.model.pnames[i])
+        fig.show()
+
+def prepEmcee():
     """ Prepares Emcee for run """
     dat = ascii.read('tser_data/timeser_1.08um_.txt',
                      names=['t','fl','flerr','model','resid'])
@@ -56,82 +182,15 @@ def prepEmcee(showGuess=False):
     y = np.array(dat['fl'])
     yerr = np.array(dat['flerr']) #* 3.
     model = sinModel()
-    ndim = model.ndim
-    nwalkers = 50
     
-    #guess = np.array([0.6,2.1,1.9,0,1])
-    guess = np.array([1.5,1.38,4.1,0.,0.995])#0.7,1.0])
-    #guess = np.array([1.5,1.38,4.1,0.,0.995])
+    guess = np.array([1.5,1.38,4.1,0.,0.995])
     spread = np.array([0.2,0.3,0.01,0.004,0.05])
     
+    mcObj = oEmcee(model,x,y,yerr,guess,spread)
     
-    if showGuess == True:
-        plt.close('all')
-        fig, ax = plt.subplots()
-        ax.errorbar(x,y,yerr=yerr,fmt='o')
-        ax.plot(x,model.evaluate(x,guess))
-        fig.show()
-    
-    p0 = []
-    for i in range(nwalkers):
-        p0.append(guess + np.random.normal(0,1.0,ndim) * spread)
+    return mcObj
 
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=[x, y,yerr,model])
-    
-    return p0, sampler, model
 
-def getMeds(sampler):
-    """ Get medians """
-    ndim = sampler.flatchain.shape[1]
-    result = []
-    for i in range(ndim):
-        result.append(np.median(sampler.flatchain[:,i]))
-    return result
-    
-def runMCMC():
-    """ Runs the MCMC
-    Example::
-    sampler, model = fit_tser_emcee.runMCMC()
-    fit_tser_emcee.plotMCMC(sampler,model)
-    
-     """
-    p0, sampler, model = prepEmcee()
-    ## Burn-in for 500 steps, then discard burn-in step
-    pos, prob, state = sampler.run_mcmc(p0,500)
-    sampler.reset()
-    pos, prob, state = sampler.run_mcmc(pos,1500)
-    return sampler, model
-    
-def plotHistory(sampler, model):
-    """ Shows the chain history """
-    plt.close('all')
-    fig, axArr = plt.subplots(model.ndim,figsize=(10,10))
-    for i, ax in enumerate(axArr):
-        for j in range(sampler.chain.shape[0]):
-            chainDat = sampler.chain[j,:,i]
-            ax.plot(chainDat)
-            ax.set_ylabel(model.pnames[i])
-    fig.show()
 
-def doCorner(sampler,model):
-    """ Runs a corner plot """
-    fig = corner.corner(sampler.flatchain,labels=model.pnames,auto_bars=False,
-                        quantiles=[0.159,0.841],show_titles=True)
-    fig.savefig('plots/corner.pdf')
 
-def plotMCMC(sampler, model):
-    """ Plots the MCMC histograms or history """
-    plt.close('all')
-    fig, axArr = plt.subplots(1,model.ndim,figsize=(17,5))
-    for i, ax in enumerate(axArr):
-        postData = sampler.flatchain[:,i]
-        ax.hist(postData,100,histtype='step')
-        ax.set_xlabel(model.pnames[i])
-        lowLimit = np.percentile(postData,5)
-        highLimit = np.percentile(postData,95)
-        showLow = lowLimit - (highLimit - lowLimit)
-        showHigh = highLimit + (highLimit - lowLimit)
-        ax.set_xlim(showLow,showHigh)
-        ax.get_yaxis().set_visible(False)
-        
-    fig.show()
+
