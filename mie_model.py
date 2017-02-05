28123a5eed0737951fcfd22e54d29c4f47ce32bf
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2017-02-04 21:32:40 -0700

    I adjusted the Mie Model so that it makes all points with a size parameter (2pi * rad/lambda) bigger than 100 the same as 100. This seems to help with the unexpected quits - seg faults?.

--- mie_model.py
+++ mie_model.py
@@ -1,6 +1,7 @@
 import numpy as np
 import miescatter
 import pdb
+import matplotlib.pyplot as plt
 
 def extinct(wavel,rad=1.0,n=complex(1.825,-1e-4),logNorm=False,
             npoint=100):
@@ -14,14 +15,14 @@ def extinct(wavel,rad=1.0,n=complex(1.825,-1e-4),logNorm=False,
     wavel: float,arr
         The array of wavelengths to evaluate
     """
-    sz = 2. * np.pi * rad/wavel
+    sz = 2. * np.pi * rad/np.array(wavel)
     
     if logNorm == True:
         ## Generate an array of points along the distribution
         ## Size multiplier
-        nwav = wavel.size
+        nwav = sz.size
         ## Size to evaluate lognormal weights
-        sizeEval = np.linspace(0.2,10.,npoint)
+        sizeEval = np.linspace(0.2,5.,npoint)
         weights = lognorm(sizeEval,0.5,1.)
         ## Arrange the array into 2D for multiplication of the grids
         sizeMult = (np.tile(sizeEval,(nwav,1))).transpose()
@@ -31,10 +32,14 @@ def extinct(wavel,rad=1.0,n=complex(1.825,-1e-4),logNorm=False,
         finalEval = eval2D.ravel() ## Evaluate a 1D array
         
     else:
-        finalEval = sz
+        finalEval = np.array(sz)
         
+    ## Make all points with sz > 100. the same as 100
+    ## Otherwise the miescatter code quits without any warning or diagnostics
+    highp = finalEval > 100.
+    finalEval[highp] = 100.
     
-    qext,qsca,qabs,qbk,qpr,alb,g = miescatter.calcscatt(n,finalEval,n=len(finalEval))
+    qext,qsca,qabs,qbk,qpr,alb,g = miescatter.calcscatt(n,finalEval,n=finalEval.size)
     
     if logNorm == True:
         ## Now sum by the weights
@@ -61,4 +66,19 @@ def lognorm(x,s,med):
     mu = np.log(med)
     y = 1. / (s*x*np.sqrt(2.*np.pi)) * np.exp(-0.5*(np.log(x-mu)/s)**2)
     return y
+    
+def compareTest():
+    """
+    Compares the Single Particle and Log-normal distributions
+    """
+    x = np.linspace(0.9,10,1024)
+    rad = 1.0
+    ysingle = extinct(x,rad=rad,logNorm=False)
+    plt.plot(x,ysingle,label='Single Particle')
+    npointA = [10,32,64,1024]
+    for npoint in npointA:
+        ymulti = extinct(x,rad=rad,logNorm=True,npoint=npoint)
+        plt.plot(x,ymulti,label='Log Normal N='+str(npoint))
+    plt.legend()
+    plt.show()
     
