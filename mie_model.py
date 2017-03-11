I made several improvements to the log-normal distribution evaluation, also corrected back to the correct linear space formula and use a logarithmic wavelength spacing.

mie_model.py
@@ -51,19 +51,23 @@ def extinct(wavel,rad=1.0,n=complex(1.825,-1e-4),logNorm=False,
         ## Size to evaluate lognormal weights
         ## Make a linear space from threshold to threshold in the PDF
         lowEval, highEval = invLognorm(s,rad,pdfThreshold)
-        sizeEval = np.linspace(lowEval,highEval,npoint)
-        weights = lognorm(sizeEval,s,1.)
-        sumWeights = np.sum(weights) * (sizeEval[1] - sizeEval[0])
-        if (sumWeights < 0.8) | (sumWeights > 1.001):
-            print('Warning, PDF weights not properly sampling PDF')
+        
+        sizeEvalExtra = np.logspace(np.log(lowEval),np.log(highEval),base=np.e,num=(npoint+1))
+        sizeEval = sizeEvalExtra[0:-1] ## extra point is for differentials
+        dSize = np.diff(sizeEvalExtra)
+        
+        weights = lognorm(sizeEval,s,rad) * dSize
+        sumWeights = np.sum(weights)
+        if (sumWeights < 0.8) | (sumWeights > 1.1):
+            print(r'!!!!!!Warning, PDF weights not properly sampling PDF!!!!')
+            pdb.set_trace()
         weights = weights / sumWeights
         ## Arrange the array into 2D for multiplication of the grids
-        sizeMult = (np.tile(sizeEval,(nwav,1))).transpose()
+        sizeMult = (np.tile(sizeEval/rad,(nwav,1))).transpose()
         sizeArr = np.tile(sz,(npoint,1))
         
         eval2D = sizeMult * sizeArr
         finalEval = eval2D.ravel() ## Evaluate a 1D array
-        pdb.set_trace()
     else:
         finalEval = np.array(sz)
         
@@ -77,7 +81,7 @@ def extinct(wavel,rad=1.0,n=complex(1.825,-1e-4),logNorm=False,
     if logNorm == True:
         ## Now sum by the weights
         qext2D = np.reshape(qext,(npoint,nwav))
-        finalQext = np.dot(weights,qext2D) * (sizeEval[1] - sizeEval[0])
+        finalQext = np.dot(weights,qext2D)
     else:
         finalQext = qext
         
@@ -86,7 +90,6 @@ def extinct(wavel,rad=1.0,n=complex(1.825,-1e-4),logNorm=False,
 def invLognorm(s,med,pdfThreshold):
     """ 
     Calculates the X values for a Log-normal distribution evaluated at specific PDF values
-    
     Arguments
     ------------------
     s: float
@@ -97,9 +100,10 @@ def invLognorm(s,med,pdfThreshold):
         The PDF threshold at which to find the x values
     """
     mu = np.log(med)
-    sqrtPart = np.sqrt(-2. * s**2 * np.log(s * np.sqrt(2. * np.pi) * pdfThreshold))
-    lowEval = np.exp(mu - sqrtPart)
-    highEval = np.exp(mu + sqrtPart)
+    z = np.log(s * np.sqrt(2. * np.pi) * pdfThreshold)
+    sqrtPart = np.sqrt((2 * s**2 - 2 * mu)**2 - 4. * mu**2 - 8. * s**2 * z)
+    lowEval = np.exp(mu - s**2 - 0.5 * sqrtPart)
+    highEval = np.exp(mu - s**2 + 0.5 * sqrtPart)
     
     return lowEval, highEval
 
@@ -117,23 +121,28 @@ def lognorm(x,s,med):
         The median particle size
     """
     mu = np.log(med)
-    y = 1. / (s*np.sqrt(2.*np.pi)) * np.exp(-0.5*((np.log(x)-mu)/s)**2)
+    y = 1. / (s* x * np.sqrt(2.*np.pi)) * np.exp(-0.5*((np.log(x)-mu)/s)**2)
     return y
 
 def showLognorm():
     """ Shows example log-normal distributions"""
-    rad = 0.5
+    rad = 1.0
     #sArray = [0.4,0.5,0.6]
-    sArray = [1.0,0.5,0.25]
+    sArray = [2.5,1.0,0.5,0.25,0.1]
     plt.close('all')
     fig, ax = plt.subplots()
     for oneS in sArray:
-        lowEval, highEval = invLognorm(oneS,rad,0.005)
-        x = np.linspace(lowEval,highEval,1024)
+        lowEval, highEval = invLognorm(oneS,rad,0.001)
+        xExtra = np.logspace(np.log(lowEval),np.log(highEval),num=1025,base=np.e)
+        x = xExtra[0:-1]
+        dx = np.diff(xExtra)
         y = lognorm(x,oneS,rad)
-        ax.plot(x,y,label='$\sigma$='+str(oneS)+', mu='+"{:.3f}".format(np.log(rad)))
-        print(lognorm(highEval,oneS,rad))
-    ax.set_xlim(0,3)
+        ax.plot(x,y,label='$\sigma$='+str(oneS)+', mu='+"{:.2f}".format(np.log(rad)))
+        
+        print('Integral='+str(np.sum(y * dx)))
+        print('Low, High='+str(lognorm(np.array([lowEval,highEval]),oneS,rad)))
+    #ax.set_xscale('log')
+    ax.set_xlim(-0.1,3)
     ax.legend(loc='best')
     fig.show()
 
@@ -150,8 +159,7 @@ def compareTest():
     for npoint in npointA:
         ymulti = extinct(x,rad=rad,logNorm=True,npoint=npoint,n=nInd)
         plt.plot(x,ymulti,label='Log Normal N='+str(npoint)+',[0.2,5]')
-    yWider = extinct(x,rad=rad,logNorm=True,npoint=256,n=nInd,
-                     lowMult=0.1,highMult=10.)
+    yWider = extinct(x,rad=rad,logNorm=True,npoint=256,n=nInd)
     plt.plot(x,yWider,label='Log normal N=256,[0.1,10]')
     plt.legend(loc='best')
     plt.show()