696fd9d4c521164b06a695c4dcca98a71102b29e
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2017-04-25 15:40:52 -0700

    I changed prep_images to objected oriented methods.

--- prep_images.py
+++ prep_images.py
@@ -7,9 +7,76 @@ import astropy.units as u
 import numpy as np
 import pdb
 
-pipePrefs = yaml.load(open('parameters/reduction_parameters.yaml'))
-rawDir = pipePrefs['rawDir']
-procDir = os.path.join(rawDir,'proc')
+class prep():
+    """ Class for reducing images """
+    def __init__(self,testMode=False):
+        self.pipePrefs = yaml.load(open('parameters/reduction_parameters.yaml'))
+        self.rawDir = self.pipePrefs['rawDir']
+        self.testMode = testMode
+        if testMode == True:
+            self.procDir = os.path.join(self.rawDir,'proc_test')
+        else:
+            self.procDir = os.path.join(self.rawDir,'proc')
+        
+    
+    def makeMasterCals(self):
+        """ Makes the master calibration files """
+    
+        for oneCal in ['biasFiles','flatFiles']:
+        
+            fileL = glob.glob(os.path.join(self.rawDir,self.pipePrefs[oneCal]))
+            if self.testMode == True:
+                fileL = fileL[0:4]
+            
+            ccdList = []
+            for oneFile in fileL:
+                head, dataCCD = getData(oneFile)
+                ccdList.append(dataCCD)
+            
+            combiner = Combiner(ccdList)
+            
+            combiner.sigma_clipping(low_thresh=2, high_thresh=5, func=np.ma.median)
+            combined_avg = combiner.average_combine()
+            
+            comb_gained = gain_correct(combined_avg,float(head['GAIN1']) * u.electron/u.adu)
+            
+            hdu = fits.PrimaryHDU(comb_gained)
+            HDUList = fits.HDUList([hdu])
+            HDUList[0].header['IMGAVG'] = ('T','Image is averaged')
+            HDUList[0].header['GAINCOR'] = ('T','Gain correction applied (units are e)')
+            HDUList[0].data = combined_avg
+            
+            if os.path.exists(self.procDir) == False:
+                os.mkdir(self.procDir)
+            
+            if oneCal == 'biasFiles':
+                outName = 'zero'
+            else:
+                outName = 'flat'
+            HDUList.writeto(os.path.join(self.procDir,'master_'+outName+'.fits'),overwrite=True)
+
+    def procSciFiles(self):
+        """ Process the science images """
+        fileL = glob.glob(os.path.join(self.rawDir,self.pipePrefs['sciFiles']))
+        if self.testMode == True:
+            fileL = fileL[0:4]
+        
+        hbias, bias = getData(os.path.join(self.procDir,'master_zero.fits'))
+        hflat, flat = getData(os.path.join(self.procDir,'master_flat.fits'))
+        
+        for oneFile in fileL:
+            head, dataCCD = getData(oneFile)
+            nccd = ccd_process(dataCCD,gain=float(head['GAIN1']) * u.electron/u.adu,
+                               master_flat=flat,
+                               master_bias=bias)
+            head['ZEROFILE'] = 'master_zero.fits'
+            head['FLATFILE'] = 'master_flat.fits'
+            head['GAINCOR'] = ('T','Gain correction applied (units are e)')
+            hdu = fits.PrimaryHDU(data=nccd,header=head)
+            HDUList = fits.HDUList([hdu])
+            newFile = os.path.basename(oneFile)
+            HDUList.writeto(os.path.join(self.procDir,newFile),overwrite=True)
+    
 
 def getData(fileName):
     """ Gets the data and converts to CCDData type"""
@@ -28,65 +95,6 @@ def getData(fileName):
     
     outData = CCDData(data,unit=outUnit)
     return head, outData
-
-def makeMasterCals(testMode=False):
-    """ Makes the master calibration files """
-    
-    for oneCal in ['biasFiles','flatFiles']:
-        
-        fileL = glob.glob(os.path.join(rawDir,pipePrefs[oneCal]))
-        if testMode == True:
-            fileL = fileL[0:4]
-        
-        ccdList = []
-        for oneFile in fileL:
-            head, dataCCD = getData(oneFile)
-            ccdList.append(dataCCD)
-        
-        combiner = Combiner(ccdList)
-        
-        combiner.sigma_clipping(low_thresh=2, high_thresh=5, func=np.ma.median)
-        combined_avg = combiner.average_combine()
-        
-        comb_gained = gain_correct(combined_avg,float(head['GAIN1']) * u.electron/u.adu)
-        
-        hdu = fits.PrimaryHDU(comb_gained)
-        HDUList = fits.HDUList([hdu])
-        HDUList[0].header['IMGAVG'] = ('T','Image is averaged')
-        HDUList[0].header['GAINCOR'] = ('T','Gain correction applied (units are e)')
-        HDUList[0].data = combined_avg
-        
-        if os.path.exists(procDir) == False:
-            os.mkdir(procDir)
-        
-        if oneCal == 'biasFiles':
-            outName = 'zero'
-        else:
-            outName = 'flat'
-        HDUList.writeto(os.path.join(procDir,'master_'+outName+'.fits'),overwrite=True)
-
-def procSciFiles(testMode=False):
-    """ Process the science images """
-    fileL = glob.glob(os.path.join(rawDir,pipePrefs['sciFiles']))
-    if testMode == True:
-        fileL = fileL[0:4]
-    
-    hbias, bias = getData(os.path.join(procDir,'master_zero.fits'))
-    hflat, flat = getData(os.path.join(procDir,'master_flat.fits'))
-    
-    for oneFile in fileL:
-        head, dataCCD = getData(oneFile)
-        nccd = ccd_process(dataCCD,gain=float(head['GAIN1']) * u.electron/u.adu,
-                           master_flat=flat,
-                           master_bias=bias)
-        hdu = fits.PrimaryHDU(nccd)
-        HDUList = fits.HDUList([hdu])
-        HDUList[0].header = head
-        HDUList[0].data = nccd
-        newFile = os.path.basename(oneFile)
-        HDUList.writeto(os.path.join(procDir,newFile),overwrite=True)
-        
-        
 
 if __name__ == "__main__":
     print("Making Master Cals")
