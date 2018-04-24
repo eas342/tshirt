599a4771e9a60d19e8404dc3196632ca8fe65be8
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2018-04-24 16:32:12 -0700

    made the kic 1255 model use a picklable interp1d function.

--- fit_models.py
+++ fit_models.py
@@ -4,6 +4,30 @@ from astropy.table import Table
 from scipy.interpolate import interp1d
 
 
+class interp1d_picklable:
+    """ class wrapper for piecewise linear function
+    Otherwise, there a problems pickling this function
+    to save MCMC results.
+    
+    Taken from:
+    https://stackoverflow.com/questions/32883491/pickling-scipy-interp1d-spline
+    """
+    def __init__(self, xi, yi, **kwargs):
+        self.xi = xi
+        self.yi = yi
+        self.args = kwargs
+        self.f = interp1d(xi, yi, **kwargs)
+
+    def __call__(self, xnew):
+        return self.f(xnew)
+
+    def __getstate__(self):
+        return self.xi, self.yi, self.args
+
+    def __setstate__(self, state):
+        self.f = interp1d(state[0], state[1], **state[2])
+        
+
 class kic1255Model:
     """ A Kepler LC model from the Kepler Short cadence data
     
@@ -16,8 +40,8 @@ class kic1255Model:
         self.kepFile = 'tser_data/reference_dat/avg_bin_kep.fits'
         self.kepDat = Table.read(self.kepFile)
         
-        self.finterp = interp1d(self.kepDat['BINMID'],self.kepDat['YBIN'] - 1.0,
-                                bounds_error=False,fill_value=0.0)
+        self.finterp = interp1d_picklable(self.kepDat['BINMID'],self.kepDat['YBIN'] - 1.0,
+                                          bounds_error=False,fill_value=0.0)
         self.ndim = len(self.pnames)
     
     def evaluate(self,x,inputP):
