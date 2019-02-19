a4d65f3b6351efa207efe460442ebebe53e4a620
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2019-02-19 13:16:26 -0700

    adding package_data to setupy.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='0.1dev',
     packages=['tser_tools',],
     license='MIE',
-#    package_data={'': ['parameters/phot_params/keywords_for_phot_pipeline.csv']},
+    package_data={'': ['parameters/phot_params/keywords_for_phot_pipeline.csv']},
     include_package_data=True,
     long_description=open('README.rst').read(),
 )
