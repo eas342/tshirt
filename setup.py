6b0de435cf47df9b70a87af3ed870bcccc4d5d1b
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2019-02-19 12:20:48 -0700

    added a setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='tser-tools',
+    version='0.1dev',
+    packages=['tser_tools',],
+    license='MIE',
+    long_description=open('README.rst').read(),
+)
