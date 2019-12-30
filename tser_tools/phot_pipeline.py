b551e3d9da75a1dfdfaa076cd5d5bedac22fc1ad
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2019-12-30 12:03:58 -0500

    no longer requires ccdproc to work

--- tser_tools/phot_pipeline.py
+++ tser_tools/phot_pipeline.py
@@ -1,5 +1,9 @@
 import photutils
-from ccdproc import CCDData, Combiner
+try:
+    from ccdproc import CCDData, Combiner
+except ImportError as err1:
+    print("Could not import ccdproc, so image processing may not work")
+
 from astropy.io import fits, ascii
 import matplotlib as mpl
 from mpl_toolkits.axes_grid1 import make_axes_locatable
