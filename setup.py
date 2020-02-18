23d9dccd4ee0168d3a20e39ea97a1510a2742267
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2020-02-18 14:34:53 -0700

    including the instrument_specific scripts in the setup file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name='tser-tools',
     version='0.1dev',
-    packages=['tser_tools',],
+    packages=['tser_tools','tser_tools.instrument_specific'],
     license='MIE',
     package_data={'': ['parameters/phot_params/keywords_for_phot_pipeline.csv'],
                   '': ['parameters/spec_params/default_params.yaml']},
