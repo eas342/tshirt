e7c4592d4984d17ce670c02bc3256feb4eec4cce
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2020-05-21 21:43:55 -0700

    adding another resource used by phot_pipeline.prevPhot

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setup(
               'tser_tools.pipeline.instrument_specific'],
     license='MIE',
     package_data={'': ['parameters/phot_params/keywords_for_phot_pipeline.csv'],
-                  '': ['parameters/spec_params/default_params.yaml']},
+                  '': ['parameters/spec_params/default_params.yaml'],
+                  '': ['parameters/phot_params/keywords_for_phot_pipeline.csv']},
     include_package_data=True,
     long_description=open('README.rst').read(),
 )
