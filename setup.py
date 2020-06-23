12802d81dd6620bd6d419c85ec87daf81ad304bb
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2020-06-23 11:22:26 -0700

    updated setup.py to new tshirt name

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 #from setuptools import setup, find_packages
 
 setup(
-    name='tser-tools',
+    name='tshirt',
     version='0.1dev',
-    packages=['tser_tools','tser_tools.pipeline',
-              'tser_tools.pipeline.instrument_specific'],
+    packages=['tshirt','tshirt.pipeline',
+              'tshirt.pipeline.instrument_specific'],
     license='MIE',
     package_data={'': ['parameters/phot_params/keywords_for_phot_pipeline.csv'],
                   '': ['parameters/spec_params/default_params.yaml'],
