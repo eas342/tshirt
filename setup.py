2366beb2700c36e7e995ea74e14ee704c35ec7f8
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2020-06-24 11:27:21 -0700

    edit to setup.py package_data dictionary to name which package gets which data

--- setup.py
+++ setup.py
@@ -7,9 +7,7 @@ setup(
     packages=['tshirt','tshirt.pipeline',
               'tshirt.pipeline.instrument_specific'],
     license='MIE',
-    package_data={'': ['parameters/phot_params/keywords_for_phot_pipeline.csv'],
-                  '': ['parameters/spec_params/default_params.yaml'],
-                  '': ['parameters/phot_params/keywords_for_phot_pipeline.csv']},
-    include_package_data=True,
+    package_data={'tshirt': ['parameters/phot_params/keywords_for_phot_pipeline.csv'],
+                  'tshirt': ['parameters/spec_params/default_params.yaml']},
     long_description=open('README.rst').read(),
 )
