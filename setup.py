85e44d8d6da99c60fde594fd38844136f541476d
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2020-06-29 22:47:22 -0700

    adjusting setup.py to work with pip

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
-from distutils.core import setup
-#from setuptools import setup, find_packages
+#from distutils.core import setup
+from setuptools import setup#, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='tshirt',
     version='0.1dev',
+    author='Everett Schlawin',
     packages=['tshirt','tshirt.pipeline',
               'tshirt.pipeline.instrument_specific'],
-    license='MIE',
+    url="https://github.com/eas342/tshirt",
+    description="A package to analyze time series data, especially for exoplanets",
     package_data={'tshirt': ['parameters/phot_params/keywords_for_phot_pipeline.csv'],
                   'tshirt': ['parameters/spec_params/default_params.yaml']},
-    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
 )
