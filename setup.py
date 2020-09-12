3823e2bb8e68ba31d43b37408d5860fabf56fcd6
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2020-09-11 20:39:18 -0700

    setting up the long description for Pip

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@
 from setuptools import setup#, find_packages
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    long_description = "".join(fh.readlines()[4:])
 
 setup(
     name='tshirt',
-    version='0.1dev2',
+    version='0.1dev3',
     author='Everett Schlawin, Kayli Glidic',
     packages=['tshirt','tshirt.pipeline',
               'tshirt.pipeline.instrument_specific'],
     url="https://github.com/eas342/tshirt",
     description="A package to analyze time series data, especially for exoplanets",
     include_package_data=True,
+    long_description=long_description,
     long_description_content_type='text/markdown'
 )
