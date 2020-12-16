62664a289036603d71a02dbd7f848374ca90cdaa
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2020-12-16 14:00:22 -0700

    updating the directories and URL for the new tshirt_example_data repository to house the example data

--- tshirt/pipeline/phot_pipeline.py
+++ tshirt/pipeline/phot_pipeline.py
@@ -1855,7 +1855,7 @@ def get_tshirt_example_data():
     data_list_file = resource_filename('tshirt','directory_info/example_data_list.txt')
     with open(data_list_file) as dlf:
         fileList = dlf.read().splitlines()
-    onlineDir = 'https://raw.githubusercontent.com/eas342/tshirt/removeExampleData/example_tshirt_data/'
+    onlineDir = 'https://raw.githubusercontent.com/eas342/tshirt/master/example_tshirt_data'
     
     example_tshirt_dir = os.path.join(baseDir,'example_tshirt_data','example_data')
     for oneFile in fileList:
@@ -1869,7 +1869,7 @@ def get_tshirt_example_data():
             if sys.version > '3':
                 f = urllib.request.urlopen(onlinePath)
             else:
-                f = urllib.open(onlinePath)
+                f = urllib.urlopen(onlinePath)
             with open(outPath,'wb') as f_out:
                 f_out.write(f.read())
             
