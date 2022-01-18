432341b8a51daaf915518aa3518c6036197700f7
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2022-01-18 15:23:01 -0700

    updated the version number for uploading to pypi

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='tshirt',
-    version='0.1dev7',
+    version='0.1dev8',
     author='Everett Schlawin, Kayli Glidic',
     packages=['tshirt','tshirt.pipeline',
               'tshirt.pipeline.instrument_specific'],
