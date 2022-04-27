c0f90141beca5d6055e6e1a442c97f96b13cc491
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2022-04-26 22:49:23 -0700

    docs/conf.py now specifies requirements

--- docs/conf.py
+++ docs/conf.py
@@ -59,3 +59,8 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Explicitly set the version of Python and its requirements
+python:
+  install:
+    - requirements: docs/requirements.txt
